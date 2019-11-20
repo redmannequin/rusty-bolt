e29cc45bfaf1edc678579c04c3d1e113b669f840
Flavio B <barinas.flavio@gmail.com>
2019-11-20 12:04:27 -0500

    refactor to lib

--- bin/src/main.rs
+++ bin/src/main.rs
@@ -1,26 +1,3 @@
-//////////////// LOGGING ///////////////////
-
-use std::io::{stderr, Write};
-
-extern crate log;
-use log::{LogRecord, LogLevel, LogMetadata};
-
-struct SimpleLogger;
-
-impl log::Log for SimpleLogger {
-    fn enabled(&self, metadata: &LogMetadata) -> bool {
-        metadata.level() <= LogLevel::Debug
-    }
-
-    fn log(&self, record: &LogRecord) {
-        if self.enabled(record.metadata()) {
-            let _ = writeln!(stderr(), "[{}]  {}", record.level(), record.args());
-        }
-    }
-}
-
-//////////////////////////////////////////////////////////////////////
-
 use std::collections::HashMap;
 use std::env;
 
@@ -32,14 +9,9 @@ fn main() {
 
     let statement = match args.nth(1) {
         Some(string) => string,
-        _ => String::from("MERGE p=(a:Person {name:'Alice'})-[r:KNOWS]->(b:Person {name:'Bob'}) RETURN a, r, p"),
+        _ => String::from("RETURN $x"),
     };
     let parameters = parameters!("x" => 1);
-
-    //    let _ = log::set_logger(|max_log_level| {
-    //        max_log_level.set(log::LogLevelFilter::Debug);
-    //        Box::new(SimpleLogger)
-    //    });
 
     let session = Neo4jDB::connect("[::1]:7687", "neo4j", "password").unwrap();
     dump(session, &statement[..], parameters);
